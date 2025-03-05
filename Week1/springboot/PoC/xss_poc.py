3a8c4dc4407d2b9abf45a5447355d6330233942d
Michael Moon <44055669+twmoon@users.noreply.github.com>
2025-03-05 23:06:56 +0900

    Update xss_poc.py

--- Week1/springboot/PoC/xss_poc.py
+++ Week1/springboot/PoC/xss_poc.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service 
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import urllib.parse
 import time
